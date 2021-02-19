a494d236d8b1208af17269baee9c4c4532ba49e8
Christopher Weyand <christopher.weyand@hpi.de>
2021-02-19 16:28:23 +0100

    try stuff

--- cluster_editing/exact/lower_bounds.cpp
+++ cluster_editing/exact/lower_bounds.cpp
@@ -66,80 +66,124 @@ struct Triple {
     }
 };
 
-int packing_local_search_bound(const Instance& inst, int limit) {
-    auto potential = inst.edges;
+int packingCost(const vector<Triple> & packing) {
+    int cost = 0;
+    for (auto & triple : packing)
+        cost += triple.cost;
 
+    return cost;
+}
+
+void maximizePacking(vector<Triple>& packing, Edges& potential) {
+    mt19937 gen{random_device{}()};
     int n = size(potential);
-    long long cost = 0; // maintains sum of packing[i].cost
-
+    vector perm(n,0);
+    iota(begin(perm), end(perm), 0);
+    shuffle(begin(perm), end(perm), gen); // TODO make this better
     // build initial packing
-    vector<Triple> packing;
-    for(int u=0; u<n; ++u) {
-        for(int v=0; v<n; ++v) {
+    for(int iu=0; iu<n; ++iu) {
+        auto u = perm[iu];
+        for(int iv=0; iv<n; ++iv) {
+            auto v = perm[iv];
             auto& uv = potential[u][v];
-            for(int w=v+1; w<n && uv>0; ++w) {
+            for(int iw=iv+1; iw<n && uv>0; ++iw) {
+                auto w = perm[iw];
                 auto& uw = potential[u][w];
                 auto& vw = potential[v][w];
                 if(uw<=0 || vw>=0) continue;
                 Triple t(u,v,w,potential);
                 assert(t.valid);
-                cost += t.apply(potential);
+                t.apply(potential);
                 packing.push_back(t);
-                if(cost>limit) return cost;
             }
         }
     }
+}
 
+void swapLocal(vector<Triple>& packing, Edges& potential) {
+    int n = size(potential);
     mt19937 gen(1337);
     uniform_real_distribution<> dist; // [0,1)
 
+
     // local search part
     int num_no_improvement = 0;
-    for(int iter=0; iter<INF && cost<=limit && num_no_improvement<5; ++iter) {
+    auto cost = packingCost(packing);
+    for (int iter = 0; iter < INF && num_no_improvement < 5; ++iter) {
 
         bool has_improved = false;
 
-        for(int i=0; i<size(packing); ++i) {
+        for (int i = 0; i < size(packing); ++i) {
             // try to replace this triple in packing
-            auto [u,v,w,uvw_cost,valid] = packing[i];
+            auto[u, v, w, uvw_cost, valid] = packing[i];
             // undo triple
             cost += packing[i].apply(potential, true);
 
-            vector<pair<Triple,int>> alternatives; // 0-3 elemns
-            for(auto [a,b] : {pair(u,v), pair(u,w), pair(v,w)}) {
+            vector<pair<Triple, int>> alternatives; // 0-3 elemns
+            for (auto[a, b] : {pair(u, v), pair(u, w), pair(v, w)}) {
 
-                tuple<Triple,double,int> mx;
-                auto& [best,rand,cand] = mx;
-                for(int x=0; x<n; ++x) {
-                    if(x==u||x==v||x==w) continue;
-                    Triple t(a,b,x,potential);
-                    if(!t.valid) continue;
-                    mx = max(mx, tuple(t,dist(gen),x));
+                tuple<Triple, double, int> mx;
+                auto&[best, rand, cand] = mx;
+                for (int x = 0; x < n; ++x) {
+                    if (x == u || x == v || x == w) continue;
+                    Triple t(a, b, x, potential);
+                    if (!t.valid) continue;
+                    mx = max(mx, tuple(t, dist(gen), x));
                 }
-                for(auto& [t,x] : alternatives)
+                for (auto&[t, x] : alternatives)
                     best.valid &= cand != x;
-                if(best.valid)
-                    alternatives.push_back(pair(best,cand));
+                if (best.valid)
+                    alternatives.push_back(pair(best, cand));
             }
 
             int sum = 0;
-            for(auto& [t,x] : alternatives) sum += t.cost;
+            for (auto&[t, x] : alternatives) sum += t.cost;
 
-            if(uvw_cost>sum || (uvw_cost==sum && dist(gen)<0.7)) { // uvw was good
+            if (uvw_cost > sum || (uvw_cost == sum && dist(gen) < 0.7)) { // uvw was good
                 cost += packing[i].apply(potential); // do not replace uvw
                 continue;
             }
 
             // replace uvw
-            has_improved = uvw_cost<sum;
-            for(auto& [t,x] : alternatives) cost += t.apply(potential);
+            has_improved = uvw_cost < sum;
+            for (auto&[t, x] : alternatives) cost += t.apply(potential);
             packing[i] = alternatives.back().first;
             alternatives.pop_back();
-            for(auto& [t,x] : alternatives) packing.push_back(t);
+            for (auto&[t, x] : alternatives) packing.push_back(t);
         }
 
-        num_no_improvement = has_improved ? 0 : num_no_improvement+1;
+        num_no_improvement = has_improved ? 0 : num_no_improvement + 1;
 
+    }
+}
+
+int packing_local_search_bound(const Instance& inst, int limit) {
+    // TODO use limit
+    mt19937 gen(random_device{}());
+    uniform_real_distribution<> dist; // [0,1)
+
+    // build initial packing
+    vector<Triple> packing;
+    auto potential = inst.edges;
+    maximizePacking(packing, potential);
+    swapLocal(packing, potential);
+    auto cost = packingCost(packing);
+
+    for(int throws = 0; throws<2; ++throws) {
+
+        vector<Triple> nextPacking;
+        nextPacking.reserve(packing.size());
+        auto next_potential = inst.edges;
+        for(auto& t : packing) if(dist(gen)<0.8) nextPacking.push_back(t), t.apply(next_potential);
+        maximizePacking(nextPacking, next_potential);
+        swapLocal(nextPacking, next_potential);
+        auto next_cost = packingCost(nextPacking);
+
+        if(next_cost > cost) {
+            packing = nextPacking;
+            potential = next_potential;
+            cost = next_cost;
+        }
     }
 
     return cost;
