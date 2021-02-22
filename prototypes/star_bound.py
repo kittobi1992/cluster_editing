6ecd797374d4936766b3fc3c3a398dd9df74e02b
Michael Hamann <michael.hamann@kit.edu>
2021-02-22 14:58:46 +0100

    Star bound prototype: add real local search including plateau search
    There still seems to be a bug where we try to remove something that is
    no longer in the bound in the candidate checking phase, but this
    should not impact the correctness.

--- prototypes/star_bound.py
+++ prototypes/star_bound.py
@@ -5,6 +5,7 @@ import itertools
 import random
 import collections
 import copy
+from enum import Enum
 
 def readGraph(path):
     problem_read = False
@@ -158,6 +159,36 @@ def lowerBound(g):
     #print(stars)
     return bound
 
+class P3PerPair:
+    def __init__(self, g):
+        self.num_p3 = collections.defaultdict(int)
+
+        # Count p3 with central node u
+        for u in g:
+            for v in g[u]:
+                for w in g[u] - g[v]:
+                    if v < w:
+                        self.num_p3[(u, v)] += 1
+                        self.num_p3[(v, u)] += 1
+                        self.num_p3[(v, w)] += 1
+                        self.num_p3[(w, v)] += 1
+                        self.num_p3[(u, w)] += 1
+                        self.num_p3[(w, u)] += 1
+
+    def __getitem__(self, e):
+        if e in self.num_p3:
+            return self.num_p3[e]
+        return 0
+
+
+class CandidateType(Enum):
+    P3 = 1
+    STAR_EXTENSION = 2
+
+
+Candidate = collections.namedtuple('Candidate', ['candidate_type',
+                                                 'nodes', 'shared_p3'])
+
 
 class LowerBound:
     def __init__(self, g):
@@ -167,6 +198,9 @@ class LowerBound:
         self.bound = 0
         self.stars = [set() for u in g]
         self.free_edges_g = copy.deepcopy(g)
+        # Dict mapping center, external node to the star it is in
+        self.stars_for_edge = dict()
+        self.p3_count = P3PerPair(g)
 
     def free_neighbors(self, u):
         return self.free_edges_g[u]
@@ -174,8 +208,11 @@ class LowerBound:
     def pair_used(self, u, v):
         return (u, v) in self.used_pairs
 
+    def canonical_star(self, star):
+        return (star[0],) + tuple(sorted(star[1:]))
+
     def has_star(self, star):
-        star = tuple(star)
+        star = self.canonical_star(star)
         return star in self.stars[star[0]]
 
     def stars_in_random_order(self):
@@ -187,15 +224,28 @@ class LowerBound:
         return not any(p in self.used_pairs
                        for p in itertools.combinations(star, 2))
 
+    def can_add_candidate(self, candidate):
+        if candidate.candidate_type == CandidateType.P3:
+            return self.can_add(candidate.nodes)
+        assert candidate.candidate_type == CandidateType.STAR_EXTENSION
+        for x in candidate.nodes[:-1]:
+            if (x, candidate.nodes[-1]) in self.used_pairs:
+                return False
+        return True
+
     def add_star(self, star):
         assert len(star) > 2
-        star = tuple(star)
+        star = self.canonical_star(star)
         for i, u in enumerate(star[1:]):
             assert star[0] in self.g[u]
             for v in star[i+2:]:
                 assert v not in self.g[u]
         self.stars[star[0]].add(star)
         self.bound += len(star) - 2
+
+        for u in star[1:]:
+            self.stars_for_edge[(star[0], u)] = star
+
         for x, y in itertools.combinations(star, 2):
             for u, v in [(x, y), (y, x)]:
                 assert (u, v) not in self.used_pairs
@@ -204,23 +254,156 @@ class LowerBound:
                     self.free_edges_g[u].remove(v)
 
     def remove_star(self, star):
-        star = tuple(star)
+        star = self.canonical_star(star)
         self.stars[star[0]].remove(star)
         self.bound -= len(star) - 2
+        for u in star[1:]:
+            del self.stars_for_edge[(star[0], u)]
         for x, y in itertools.combinations(star, 2):
             for u, v in [(x, y), (y, x)]:
                 self.used_pairs.remove((u, v))
                 if v in self.g[u]:
                     self.free_edges_g[u].add(v)
 
-    def get_candidates(self, u, v):
-        if v in self.g[u]:
-            return [(v, u, y) for y in self.free_edges_g[v] if y != u and
-                    y not in self.g[u] and (u, y) not in self.used_pairs] + \
-                   [(u, v, y) for y in self.free_edges_g[u] if y != v and
-                    y not in self.g[v] and (v, y) not in self.used_pairs]
-        return [(y, v, u) for y in self.free_edges_g[u] & self.free_edges_g[v]]
+    def add_candidate(self, candidate):
+        if candidate.candidate_type == CandidateType.P3:
+            self.add_star(candidate.nodes)
+        else:
+            assert candidate.candidate_type == CandidateType.STAR_EXTENSION
+            existing_star = candidate.nodes[:-1]
+            self.remove_star(existing_star)
+            self.add_star(candidate.nodes)
 
+    def remove_candidate(self, candidate):
+        if candidate.candidate_type == CandidateType.P3:
+            self.remove_star(candidate.nodes)
+        else:
+            assert candidate.candidate_type == CandidateType.STAR_EXTENSION
+            existing_star = candidate.nodes[:-1]
+            self.remove_star(candidate.nodes)
+            self.add_star(existing_star)
+
+    def get_candidates(self, u, v):
+        star_extensions = []
+        if v in self.g[u]:
+            p3s = [(v, u, y) for y in self.free_edges_g[v] if y != u and
+                   y not in self.g[u] and (u, y) not in self.used_pairs] + \
+                  [(u, v, y) for y in self.free_edges_g[u] if y != v and
+                   y not in self.g[v] and (v, y) not in self.used_pairs]
+
+            for center, ext in [(u, v), (v, u)]:
+                for star in self.stars[center]:
+                    for x in star[1:]:
+                        if self.pair_used(ext, x) or x in self.g[ext]:
+                            break
+                    else:
+                        star_extensions.append(
+                            Candidate(CandidateType.STAR_EXTENSION,
+                                      star + (ext,),
+                                      sum((self.p3_count[(ext, x)]
+                                           for x in star))))
+        else:
+            p3s = [(y, v, u) for y in self.free_edges_g[u] &
+                   self.free_edges_g[v]]
+
+            for ext, existing in [(u, v), (v, u)]:
+                for center in self.free_edges_g[ext]:
+                    if (center, existing) in self.stars_for_edge:
+                        star = self.stars_for_edge[(center, existing)]
+
+                        for x in star[1:]:
+                            if self.pair_used(ext, x) or x in self.g[ext]:
+                                break
+                        else:
+                            star_extensions.append(
+                                Candidate(CandidateType.STAR_EXTENSION,
+                                          star + (ext,),
+                                          sum((self.p3_count[(ext, x)]
+                                               for x in star))))
+
+        return star_extensions + \
+            [Candidate(CandidateType.P3, nodes,
+                       sum((self.p3_count[e]
+                            for e in itertools.combinations(nodes, 2))))
+             for nodes in p3s]
+
+
+def tryImprove(bound, star):
+    # First attempt: merge with another star
+    for star2 in bound.stars[star[0]]:
+        if star == star2:
+            continue
+
+        for u, v in itertools.product(star[1:], star2[1:]):
+            if bound.pair_used(u, v) or v in bound.g[u]:
+                break
+        else:
+            print("merged stars {} and {}".format(star, star2))
+            bound.remove_star(star)
+            bound.remove_star(star2)
+            bound.add_star(star + star2[1:])
+            return
+
+    candidate_star = list(star)
+    # Remove nodes one by one instead of removing all
+    for v in list(star[1:]):  # Force a copy
+        # Try removing v from star and inserting at least two P3
+        bound.remove_star(candidate_star)
+
+        is_p3 = len(candidate_star) == 3
+
+        if not is_p3:
+            assert len(candidate_star) > 3
+            candidate_star.remove(v)
+            bound.add_star(candidate_star)
+
+        candidates_per_pair = dict()
+        if is_p3:
+            for x, y in itertools.combinations(candidate_star, 2):
+                candidates_per_pair[(x, y)] = bound.get_candidates(x, y)
+        else:
+            for x in candidate_star:
+                assert v not in bound.g[x] or x == star[0]
+                candidates_per_pair[(v, x)] = bound.get_candidates(v, x)
+
+        two_found = False
+        for pair, candidates in candidates_per_pair.items():
+            assert not two_found
+            for candidate in candidates:
+                assert not two_found
+                bound.add_candidate(candidate)
+
+                for pair2, candidates2 in candidates_per_pair.items():
+                    if pair == pair2:
+                        continue
+                    for candidate2 in candidates2:
+                        if bound.can_add_candidate(candidate2):
+                            two_found = True
+                            bound.add_candidate(candidate2)
+                if two_found:
+                    break
+                bound.remove_candidate(candidate)
+            if two_found:
+                break
+        # Re-insert v
+        if not two_found:
+            # Replace by random candidate
+            all_candidates = list(set((c for c in candidates for candidates in candidates_per_pair.values())))
+            if all_candidates:
+                if random.random() < 0.8:
+                    replacement = min(all_candidates, key=lambda x: x.shared_p3)
+                else:
+                    replacement = random.choice(all_candidates)
+                if sorted(replacement.nodes) == sorted(candidate_star + [v]):
+                    candidate_star.append(v)
+                bound.add_candidate(replacement)
+            else:
+                if not is_p3:
+                    bound.remove_star(candidate_star)
+                    candidate_star.append(v)
+                bound.add_star(candidate_star)
+        if is_p3:
+            break
 
 def lowerBoundColoring(g):
     bound = LowerBound(g)
@@ -243,70 +426,24 @@ def lowerBoundColoring(g):
             star = [u] + list(star)
             bound.add_star(star)
         # print(i, u, bound)
-    for i in range(5):
+    old_bound = 0
+    num_unchanged = 0
+    while old_bound < bound.bound or num_unchanged < 5:
+        if old_bound < bound.bound:
+            num_unchanged = 0
+        else:
+            num_unchanged += 1
+        old_bound = bound.bound
         print(bound.bound)
         for star in bound.stars_in_random_order():
             # Check if for some reason we have removed this star
             if not bound.has_star(star):
                 continue
 
-            candidate_star = list(star)
-            # Remove nodes one by one instead of removing all
-            for v in list(star[1:]):  # Force a copy
-                # Try removing v from star and inserting at least two P3
-                bound.remove_star(candidate_star)
+            tryImprove(bound, star)
 
-                is_p3 = len(candidate_star) == 3
-
-                if not is_p3:
-                    assert len(candidate_star) > 3
-                    candidate_star.remove(v)
-                    bound.add_star(candidate_star)
-
-                candidates_per_pair = dict()
-                if is_p3:
-                    for x, y in itertools.combinations(candidate_star, 2):
-                        candidates_per_pair[(x, y)] = bound.get_candidates(x, y)
-                else:
-                    for x in candidate_star:
-                        assert v not in g[x] or x == star[0]
-                        candidates_per_pair[(v, x)] = bound.get_candidates(v, x)
-
-                two_found = False
-                for pair, candidates in candidates_per_pair.items():
-                    assert not two_found
-                    for candidate in candidates:
-                        assert not two_found
-                        bound.add_star(candidate)
-
-                        for pair2, candidates2 in candidates_per_pair.items():
-                            if pair == pair2:
-                                continue
-                            for candidate2 in candidates2:
-                                if bound.can_add(candidate2):
-                                    two_found = True
-                                    bound.add_star(candidate2)
-                        if two_found:
-                            break
-                        bound.remove_star(candidate)
-                    if two_found:
-                        break
-                # Re-insert v
-                if not two_found:
-                    # Replace by random candidate
-                    all_candidates = list(set((c for c in candidates for candidates in candidates_per_pair.values())))
-                    if all_candidates:
-                        replacement = random.choice(all_candidates)
-                        bound.add_star(replacement)
-                    else:
-                        if not is_p3:
-                            bound.remove_star(candidate_star)
-                            candidate_star.append(v)
-                        bound.add_star(candidate_star)
-                if is_p3:
-                    break
     return bound.bound
 
 
-graph = readGraph("/home/michael/graphs/PACE2021/exact/exact191.gr")
+graph = readGraph("/home/michael/graphs/PACE2021/exact/exact149.gr")
 lowerBoundColoring(graph)
