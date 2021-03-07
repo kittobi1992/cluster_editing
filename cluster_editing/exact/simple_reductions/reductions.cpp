3a0c8898f1d9373df6511431e48ca361c8ccb4cd
Connor Cates <35548067+ccates83@users.noreply.github.com>
2021-03-07 14:33:38 -0500

    here are the graphs

--- cluster_editing/exact/simple_reductions/reductions.cpp
+++ cluster_editing/exact/simple_reductions/reductions.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 
+using namespace std;
+
+
+bool heavy_non_edge_rule() {
+
+
+  return false;
+}
+
 int main(int argc,  char **argv) {
   std::cout << "Implementing data reduction rules..." << std::endl;
 
