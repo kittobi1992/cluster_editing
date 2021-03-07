3bf9119f411b160aa9b8ff161f019782b4c52a0e
Connor Cates <35548067+ccates83@users.noreply.github.com>
2021-03-07 15:18:14 -0500

    added a test graph

--- cluster_editing/exact/simple_reductions/reductions.cpp
+++ cluster_editing/exact/simple_reductions/reductions.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 double INF = numeric_limits<double>::infinity();
 
+
 /*
   Rule 1: heavy non edge rule
     Set an edge to FORBIDDEN (weight -INF) if the degree of the node is greadter \
@@ -24,7 +25,7 @@ void heavy_non_edge_rule(vector<vector<int> > &graph, int u, int v) {
 
   if (abs(graph[u][v]) >= sum) {
     cout << "FORBIDDEN: (" << u << ", " << v << ")" << endl;
-    graph[u][v] = -INF;
+    // graph[u][v] = -INF;
   }
 }
 
@@ -69,7 +70,7 @@ void heavy_edge_single_end_rule(vector<vector<int> > &graph, int u, int v) {
 */
 void heavy_edge_both_end_rule(vector<vector<int> > &graph, int u, int v) {
   int sum_u = 0;
-  int sum_v = 0
+  int sum_v = 0;
   for (int w = 0; w < graph.size(); w++) {
     if (w != v)
       sum_u += graph[u][w];
@@ -82,7 +83,39 @@ void heavy_edge_both_end_rule(vector<vector<int> > &graph, int u, int v) {
   }
 }
 
+void print(std::vector<std::vector<int> > matrix) {
+  for (int i = 0 ; i < matrix.size(); i++) {
+    cout << "[";
+    for (int j = 0; j < matrix[i].size(); j++) {
+      cout << matrix[i][j] << " ";
+    }
+    cout << "]" << endl;
+  }
+}
+
 int main(int argc,  char **argv) {
   std::cout << "Implementing data reduction rules..." << std::endl;
+
+  std::vector<std::vector<int> > test_gr(5, std::vector<int>(5, 0));
+  for (int i = 0 ; i < test_gr.size(); i++) {
+    for (int j = 0; j < test_gr[i].size(); j++) {
+      if ((i + j) % 2 == 1) {
+        test_gr[i][j] = 1;
+      }
+    }
+  }
+  print(test_gr);
+  cout << endl;
+
+
+  cout << "\nRule 1:" << endl;
+  heavy_non_edge_rule(test_gr, 0, 1);
+
+  cout << "\nRule 2:" << endl;
+  heavy_edge_single_end_rule(test_gr, 0, 1);
+
+  cout << "\nRule 3:" << endl;
+  heavy_edge_single_end_rule(test_gr, 0, 1);
+
   return 0;
 }
