eb6d0628888fda792f495f2e4e5d2e1eaa1fce93
Tobias Heuer <tobias.heuer@gmx.net>
2021-05-23 06:40:38 +0200

    minor

--- cluster_editing/application/cluster_editing.cc
+++ cluster_editing/application/cluster_editing.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <set>
 #include <signal.h>
 
 #include "cluster_editing/definitions.h"
@@ -7,7 +8,6 @@
 #include "cluster_editing/utils/timer.h"
 #include "cluster_editing/utils/randomize.h"
 #include "cluster_editing/metrics.h"
-#include "cluster_editing/io/command_line_options.h"
 #include "cluster_editing/io/output.h"
 #include "cluster_editing/io/graph_io.h"
 
@@ -19,19 +19,57 @@ Graph graph;
 HighResClockTimepoint start, end;
 
 void printResult(Graph& best) {
-  // Print Stats
-  end = std::chrono::high_resolution_clock::now();
-  std::chrono::duration<double> elapsed_seconds(end - start);
-  io::printClusterEditingResults(best, context, elapsed_seconds);
-
-  // Print RESULT line
   if ( context.general.print_result_line ) {
-    io::printResultLine(best, context, elapsed_seconds);
-  }
+    // Deletions
+    std::vector<std::vector<NodeID>> cliques(best.numNodes());
+    for ( const NodeID u : best.nodes() ) {
+      cliques[best.clique(u)].push_back(u);
+      for ( const NodeID& v : best.neighbors(u) ) {
+        if ( u < v && best.clique(u) != best.clique(v) ) {
+          std::cout << (u + 1) << " " << (v + 1) << std::endl;
+        }
+      }
+    }
 
-  if ( context.general.print_csv ) {
-    std::cout << context.general.graph_filename.substr(context.general.graph_filename.find_last_of('/') + 1)
-              << "," << metrics::edits(best) << "," << elapsed_seconds.count() << std::endl;
+    // Insertions
+    for ( CliqueID c = 0; c < best.numNodes(); ++c ) {
+      if ( cliques[c].size() > 0 ) {
+        std::set<NodeID> clique(cliques[c].begin(), cliques[c].end());
+
+        for ( const NodeID u : cliques[c] ) {
+          for ( const NodeID& v : best.neighbors(u) ) {
+            if ( best.clique(u) == best.clique(v) ) {
+              clique.erase(v);
+            }
+          }
+
+          for ( const NodeID v : clique ) {
+            if ( u < v ) {
+              std::cout << (u + 1) << " " << (v + 1) << std::endl;
+            }
+          }
+
+          for ( const NodeID& v : best.neighbors(u) ) {
+            if ( best.clique(u) == best.clique(v) ) {
+              clique.insert(v);
+            }
+          }
+        }
+      }
+    }
+  } else {
+    // Print Stats
+    end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed_seconds(end - start);
+    io::printClusterEditingResults(best, context, elapsed_seconds);
+
+    // Print RESULT line
+    io::printResultLine(best, context, elapsed_seconds);
+
+    if ( context.general.print_csv ) {
+      std::cout << context.general.graph_filename.substr(context.general.graph_filename.find_last_of('/') + 1)
+                << "," << metrics::edits(best) << "," << elapsed_seconds.count() << std::endl;
+    }
   }
 
   if ( context.general.write_to_file ) {
@@ -50,18 +88,85 @@ void terminate(int) {
   }
 }
 
-int main(int argc, char* argv[]) {
+int main() {
   // Register signal handler
   struct sigaction action;
   memset(&action, 0, sizeof(struct sigaction));
   action.sa_handler = terminate;
   sigaction(SIGTERM, &action, NULL);
 
-  processCommandLineInput(context, argc, argv);
+  // General Options
+  context.general.verbose_output = false;
+  context.general.print_result_line = true;
+  context.general.write_to_file = false;
+  context.general.read_from_file = false;
+  context.general.seed = 1;
+  context.general.num_repititions = 1000;
+  context.general.num_fruitless_repititions = 10;
+  context.general.time_limit = 590; // 9 min and 50 secs
   utils::Randomize::instance().setSeed(context.general.seed);
 
+  // Evolutionary Options
+  context.refinement.use_evo = true;
+  context.refinement.evo.enable_detailed_output = false;
+  context.refinement.evo.time_limit = 90; // 90 seconds
+  context.refinement.evo.solution_pool_size = 1;
+  context.refinement.evo.evolutionary_steps = 1000;
+  context.refinement.evo.initial_lp_iterations = 100;
+  context.refinement.evo.initial_node_swapper_iterations = 20;
+  context.refinement.evo.intensivate_lp_iterations = 25;
+  context.refinement.evo.lp_iterations_after_mutate = 50;
+  context.refinement.evo.clique_remover_iterations = 10;
+  context.refinement.evo.clique_splitter_iterations = 10;
+  context.refinement.evo.node_swapper_iterations = 5;
+  context.refinement.evo.node_swapper_max_cluster_size = 32;
+  context.refinement.evo.use_random_node_ordering = false;
+  context.refinement.evo.enable_all_mutations_after_steps = 500;
+  context.refinement.evo.enabled_mutations = "001110";
+  context.refinement.evo.large_clique_threshold = 3;
+  context.refinement.evo.min_clique_isolate_prob = 0.01;
+  context.refinement.evo.max_clique_isolate_prob = 0.1;
+  context.refinement.evo.min_neighbor_clique_isolate_prob = 0.0001;
+  context.refinement.evo.max_neighbor_clique_isolate_prob = 0.01;
+  context.refinement.evo.min_node_isolation_prob = 0.01;
+  context.refinement.evo.max_node_isolation_prob = 0.25;
+  context.refinement.evo.min_node_move_prob = 0.01;
+  context.refinement.evo.max_node_move_prob = 0.25;
+  context.refinement.evo.min_clique_split_mutation_prob = 0.01;
+  context.refinement.evo.max_clique_split_mutation_prob = 0.25;
+  context.refinement.evo.min_test_mutation_prob = 0.01;
+  context.refinement.evo.max_test_mutation_prob = 0.25;
+
+  // Evolutionary Options
+  context.refinement.use_localized_evo = true;
+  context.refinement.localized_evo.steps = 1000000;
+  context.refinement.localized_evo.max_lp_iterations = 3;
+  context.refinement.localized_evo.num_mutations_nodes = 20;
+  context.refinement.localized_evo.choose_adjacent_mutation_node_prob = 0.5;
+  context.refinement.localized_evo.max_distance_to_mutation_node = 1;
+  context.refinement.localized_evo.degree_sampling_threshold = 128;
+
+  // LP Refiner Options
+  context.refinement.use_lp_refiner = false;
+  context.refinement.lp.maximum_lp_iterations = 1000;
+  context.refinement.lp.random_shuffle_each_round = false;
+  context.refinement.lp.node_order = NodeOrdering::none;
+  context.refinement.lp.rating_map_degree_threshold = 16;
+  context.refinement.lp.min_target_edit_distance = 30;
+  context.refinement.lp.min_improvement = 5;
+  context.refinement.lp.early_exit_window = 100;
+
+  // FM Refiner Options
+  context.refinement.use_boundary_fm_refiner = false;
+  context.refinement.use_localized_fm_refiner = false;
+  context.refinement.fm.maximum_fm_iterations = 100;
+  context.refinement.fm.fraction_of_fruitless_moves = 0.05;
+  context.refinement.fm.num_seed_nodes = 100;
+  context.refinement.fm.min_improvement = 2;
+  context.refinement.fm.early_exit_window = 10;
+
   utils::Timer::instance().start_timer("import_graph", "Import Graph");
-  graph = io::readGraphFile(context.general.graph_filename);
+  graph = io::readGraphFile();
   utils::Timer::instance().stop_timer("import_graph");
   context.configureAlgorithm(graph);
 
@@ -78,13 +183,7 @@ int main(int argc, char* argv[]) {
   int fruitless_repititions = 0;
   std::vector<CliqueID> best_cliques(graph.numNodes(), INVALID_CLIQUE);
   size_t best_objective = std::numeric_limits<size_t>::max();
-  for ( int i = 0;
-        i < context.general.num_repititions &&
-        fruitless_repititions < context.general.num_fruitless_repititions ; ++i ) {
-    graph.reset();
-    flat::solve(graph, context);
-
-    // Check if solution is better than best solution found so far
+  auto check_for_new_best_solution = [&] {
     const size_t current_objective = metrics::edits(graph);
     if ( current_objective < best_objective ) {
       if ( context.general.verbose_output ) {
@@ -97,6 +196,18 @@ int main(int argc, char* argv[]) {
     } else {
       ++fruitless_repititions;
     }
+  };
+  auto solve = [&] {
+    graph.reset();
+    flat::solve(graph, context);
+    // Check if solution is better than best solution found so far
+    check_for_new_best_solution();
+  };
+
+  for ( int i = 0;
+        i < context.general.num_repititions &&
+        fruitless_repititions < context.general.num_fruitless_repititions ; ++i ) {
+    solve();
 
     if ( context.isTimeLimitReached() ) {
       break;
