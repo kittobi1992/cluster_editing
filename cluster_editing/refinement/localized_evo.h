a8636143ebed504731fca45debadba6cd9206771
Tobias Heuer <tobias.heuer@gmx.net>
2021-05-26 12:53:15 +0200

    minor

--- cluster_editing/refinement/localized_evo.h
+++ cluster_editing/refinement/localized_evo.h
@@ -54,9 +54,11 @@ class LocalizedEvolutionary final : public IRefiner {
     _refinement_nodes(),
     _cliques_with_same_rating(),
     _marked(graph.numNodes()),
-    _max_distance(context.refinement.localized_evo.max_distance_to_mutation_node) {
+    _max_distance(context.refinement.localized_evo.max_distance_to_mutation_node),
+    _max_mutation_nodes(context.refinement.localized_evo.max_mutations_nodes) {
     if ( utils::CommonOperations::instance(graph)._is_special_instance ) {
       _max_distance = std::max(5, _max_distance);
+      _max_mutation_nodes = std::min(5, _max_mutation_nodes);
     }
   }
 
@@ -107,5 +109,6 @@ class LocalizedEvolutionary final : public IRefiner {
   std::vector<CliqueID> _cliques_with_same_rating;
   ds::FastResetFlagArray<> _marked;
   int _max_distance;
+  int _max_mutation_nodes;
 };
 }  // namespace cluster_editing
